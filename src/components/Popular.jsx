b7846e0948c9d52c020ed3ccfa06ce042554bbb6
istvantothdesign <69763751+istvantothdesign@users.noreply.github.com>
2023-04-12 00:40:48 +0100

    Added images

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -13,19 +16,73 @@ function Popular() {
     );
     const data = await api.json();
     setPopular(data.recipes);
+    console.log(data.recipes);
   };
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide
+          options={{
+            perPage: 2,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "2rem",
+          }}
+        >
+          {popular.map((recipe, index) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-size: 1rem;
+    font-weight: 600;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 export default Popular;
